67e4dd6226726f38d1def3cf14fc723c9c39f6a1
Alexandre Pereira <alexandre.pereira.b@outlook.com>
2021-10-29 11:34:09 +1100

    Command palette design + open/close logic.

--- src/MudDemo/MudDemo.Client/Program.cs
+++ src/MudDemo/MudDemo.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudDemo.Client;
 using MudBlazor.Services;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
 builder.Services.AddMudServices();
+builder.Services.AddHotKeys();
 
 await builder.Build().RunAsync();
