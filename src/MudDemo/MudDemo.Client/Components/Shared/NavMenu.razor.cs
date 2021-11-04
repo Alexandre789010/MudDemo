86476d4103f06483b44be2009852087ad6c3cd34
Alexandre Pereira <alexandre.pereira.b@outlook.com>
2021-11-04 18:12:30 +1100

    Notification menu basic version.

--- src/MudDemo/MudDemo.Client/Components/Shared/NavMenu.razor.cs
+++ src/MudDemo/MudDemo.Client/Components/Shared/NavMenu.razor.cs
@@ -1,15 +1,24 @@
 using Microsoft.AspNetCore.Components;
 using MudDemo.Client.Models;
+using MudDemo.Client.Models.Notification;
+using MudDemo.Client.Services;
 
 namespace MudDemo.Client.Components.Shared;
 
 public partial class NavMenu
 {
+    private IEnumerable<NotificationModel> _activeNotifications;
+
+    [Inject] private INotificationsService NotificationsService { get; set; }
+
     [EditorRequired] [Parameter] public ThemeManagerModel ThemeManager { get; set; }
     [EditorRequired] [Parameter] public bool CanMiniSideMenuDrawer { get; set; }
     [EditorRequired] [Parameter] public EventCallback ToggleSideMenuDrawer { get; set; }
     [EditorRequired] [Parameter] public EventCallback OpenCommandPalette { get; set; }
-
     [EditorRequired] [Parameter] public UserModel User { get; set; }
 
+    protected override async Task OnInitializedAsync()
+    {
+        _activeNotifications = await NotificationsService.GetActiveNotifications();
+    }
 }
--- src/MudDemo/MudDemo.Client/Components/Shared/NotificationMenu.razor.cs
+++ src/MudDemo/MudDemo.Client/Components/Shared/NotificationMenu.razor.cs
@@ -3,44 +3,17 @@ using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
 using MudBlazor.Utilities;
 using MudDemo.Client.Models.Notification;
+using MudDemo.Client.Services;
 
 namespace MudDemo.Client.Components.Shared;
 
 public partial class NotificationMenu : MudComponentBase
 {
-    private List<NotificationModel> _notifications = new()
-    {
-        new NotificationModel
-        {
-            NotificationType = NotificationTypes.NewMessage,
-            Message = "Alexandre sent you a new message",
-            DateTimeStamp = DateTime.Now - TimeSpan.FromMinutes(34)
-        },
-        new NotificationModel
-        {
-            NotificationType = NotificationTypes.CommentLiked,
-            Message = "Joyce liked a message you sent.",
-            DateTimeStamp = DateTime.Now - TimeSpan.FromMinutes(54)
-        },
-        new NotificationModel
-        {
-            NotificationType = NotificationTypes.NewEmail,
-            Message = "Peter sent you an email.",
-            DateTimeStamp = DateTime.Now - TimeSpan.FromDays(2)
-        },
-        new NotificationModel
-        {
-            NotificationType = NotificationTypes.OrderPlaced,
-            Message = "Your order has been placed.",
-            DateTimeStamp = DateTime.Now - TimeSpan.FromDays(10)
-        }
-    };
-
     private string Classname =>
         new CssBuilder()
             .AddClass(Class)
             .Build();
     
-    [Parameter]
-    public EventCallback<MouseEventArgs> OnClickViewAll { get; set; }
+    [Parameter] public IEnumerable<NotificationModel>? Notifications { get; set; }
+    [Parameter] public EventCallback<MouseEventArgs> OnClickViewAll { get; set; }
 }
