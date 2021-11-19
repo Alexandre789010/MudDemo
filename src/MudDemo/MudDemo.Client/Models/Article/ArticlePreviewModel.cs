c9b3ec8793d2762142fc9d1c96b8134e09369258
Alexandre Pereira <alexandre.pereira.b@outlook.com>
2021-11-20 10:25:47 +1100

    Remove JsonProperty attribute.

--- src/MudDemo/MudDemo.Client/Models/Article/ArticlePreviewModel.cs
+++ src/MudDemo/MudDemo.Client/Models/Article/ArticlePreviewModel.cs
@@ -2,22 +2,19 @@ namespace MudDemo.Client.Models.Article;
 
 public class ArticlePreviewModel
 {
-    [JsonProperty]
     public int ArticleId { get; set; }
-    [JsonProperty]
     public string Title { get; set; }
-    [JsonProperty]
     public string Description { get; set; }
-    [JsonProperty]
+    
     public string DescriptionTrimed
     {
         get
         {
             var maxLength = 40;
-
+    
             if (Description.Length > maxLength)
                 return Description.Substring(0, maxLength) + "...";
-
+    
             return Description;
         }
     }
