3fdb626164910c9f7492e8277d625e87cc828722
neozhu <new163@163.com>
2022-01-29 17:14:58 +0800

    wip

--- src/MudDemo/MudDemo.Client/Program.cs
+++ src/MudDemo/MudDemo.Client/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using MudDemo.Client;
 using MudDemo.Client.Services;
+using System.Globalization;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -15,9 +16,25 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddMudServices();
 builder.Services.AddHotKeys();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+
 builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddTransient<INotificationsService, NotificationsService>();
 builder.Services.AddTransient<IArticlesService, ArticlesService>();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+var storageService = host.Services.GetRequiredService<ILocalStorageService>();
+if (storageService != null)
+{
+    var languageCode = await storageService.GetItemAsStringAsync("CurrentLanguage");
+    if (languageCode is not null)
+    {
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(languageCode);
+        CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(languageCode);
+    }
+    
+}
+
+await host.RunAsync();
