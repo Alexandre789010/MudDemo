3d2612dc850a704eb6b606620808ea5194260bbe
neozhu <new163@163.com>
2022-01-29 16:09:09 +0800

    add language selector component

---
+++ src/MudDemo/MudDemo.Client/Models/Localization/LanguageCode.cs
@@ -0,0 +1,24 @@
+﻿namespace MudDemo.Client.Models.Localization;
+
+public record LanguageCode(string Code, string DisplayName, bool IsRTL = false);
+
+
+
+public static class LocalizationConstants
+{
+    public static readonly LanguageCode[] SupportedLanguages =
+    {
+        new("en-US", "English", true),
+        new("fr-FR", "French"),
+        new("km_KH", "Khmer"),
+        new("de-DE", "German"),
+        new("nl-NL", "Dutch - Netherlands"),
+        new("es-ES", "Spanish"),
+        new("ru-RU", "Russian"),
+        new("sv-SE", "Swedish"),
+        new("id-ID", "Indonesia"),
+        new("it-IT", "Italian"),
+        new("zh-CN", "中文(简体)")
+    };
+}
+
